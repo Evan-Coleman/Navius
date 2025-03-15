2df1d133005b7011169f9f2258c003945174c8e0
Evan Coleman <me@ecoleman.dev>
2025-03-15 09:47:30 -0700

    add  start of automation of openapi gen

---
+++ src/openapi/generate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the path to the OpenAPI Generator CLI tool
+OPENAPI_GEN_PATH="/path/to/openapi-generator-cli.jar"  # Adjust to your OpenAPI Generator JAR location
+
+# Set the path to your OpenAPI specification file
+OPENAPI_SPEC_PATH="/path/to/openapi-spec.yaml"  # Adjust to your OpenAPI spec path
+
+# Set the path to your YAML configuration file
+CONFIG_PATH="/path/to/openapi-generator-config.yml"  # Adjust to your YAML config file path
+
+# Run OpenAPI Generator using the YAML configuration file
+openapi-generator generate -i $OPENAPI_SPEC_PATH -c $CONFIG_PATH
